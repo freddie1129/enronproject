b00fd62f1f5c4fef82c60f08f3b0f09df84ef84f
Freddie <freddiechenchen@gmail.com>
2018-07-20 18:57:05 +1000

    admin

--- enron/admin.py
+++ enron/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import Question
+from enron.models import Email, ToEmail, CcEmail, BccEmail, Sender, ReceiverTo, ReceiverCC, ReceiverBCC, StaffName, StaffEmail
 # Register your models here.
-admin.site.register(Question)
+
+class EnronAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+admin.site.register(Email,EnronAdmin)
+admin.site.register(ToEmail,EnronAdmin)
+admin.site.register(CcEmail,EnronAdmin)
+admin.site.register(BccEmail,EnronAdmin)
+admin.site.register(Sender,EnronAdmin)
+admin.site.register(ReceiverTo,EnronAdmin)
+admin.site.register(ReceiverCC,EnronAdmin)
+admin.site.register(ReceiverBCC,EnronAdmin)
+admin.site.register(StaffName,EnronAdmin)
+admin.site.register(StaffEmail,EnronAdmin)
--- enron/models.py
+++ enron/models.py
@@ -17,6 +17,7 @@ class Choice(models.Model):
 
 
 class Email(models.Model):
+
     emailId = models.CharField(max_length=100,primary_key=True)
     time = models.DateTimeField()
     fromAddress = models.CharField(max_length=200)
@@ -25,7 +26,12 @@ class Email(models.Model):
     path = models.CharField(max_length=128);
 
     def __str__(self):
-        return self.emailId + " " + self.fromAddress + "---->"
+        return self.emailId
+
+    class Meta:
+        verbose_name = "Sended Email"
+        verbose_name_plural = "Sended Emails"
+        default_permissions=('view')
 
 class ToEmail(models.Model):
     fromAddress = models.CharField(max_length=200)
@@ -33,7 +39,12 @@ class ToEmail(models.Model):
     emailId = models.ForeignKey(Email, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.emailId + " " + self.fromAddress + "---->" + self.receiverAddress
+        return self.emailId
+
+    class Meta:
+        verbose_name = "Received Email"
+        verbose_name_plural = "Received Emails"
+        default_permissions = ('view')
 
 class CcEmail(models.Model):
     fromAddress = models.CharField(max_length=200)
@@ -41,7 +52,14 @@ class CcEmail(models.Model):
     emailId = models.ForeignKey(Email, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.emailId + " " + self.fromAddress + "---->" + self.receiverAddress
+        return self.emailId
+
+    class Meta:
+        verbose_name = "CC Received Email"
+        verbose_name_plural = "CC Received Emails"
+        default_permissions = ('view')
+
+
 
 
 class BccEmail(models.Model):
@@ -50,24 +68,39 @@ class BccEmail(models.Model):
     emailId = models.ForeignKey(Email, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.emailId + " " + self.fromAddress + "---->" + self.receiverAddress
+        return self.emailId
 
+
+    class Meta:
+        verbose_name = "BCC Received Email"
+        verbose_name_plural = "CC Received Emails"
+        default_permissions = ('view')
 
 class Sender(models.Model):
     sender = models.CharField(max_length=128)
     number = models.BigIntegerField()
 
     def __str__(self):
-        return self.sender
+        return '{0} ({1})'.format(self.sender, self.number)
 
+
+    class Meta:
+        verbose_name = "Sender Email Address"
+        verbose_name_plural = "Sender Email Addresses"
+        default_permissions = ('view')
 
 class ReceiverTo(models.Model):
     name = models.CharField(max_length=128)
     number = models.BigIntegerField()
 
     def __str__(self):
-        return self.name
+        return '{0} ({1})'.format(self.name, self.number)
 
+
+    class Meta:
+        verbose_name = "Receiver Email Address"
+        verbose_name_plural = "Receiver Email Addresses"
+        default_permissions = ('view')
 
 
 class ReceiverCC(models.Model):
@@ -75,19 +108,40 @@ class ReceiverCC(models.Model):
     number = models.BigIntegerField()
 
     def __str__(self):
-        return self.name
+        return '{0} ({1})'.format(self.name, self.number)
+
+
+    class Meta:
+        verbose_name = "CC Receiver Email Address"
+        verbose_name_plural = "CC Receiver Email Addresses"
+        default_permissions = ('view')
+
 
 class ReceiverBCC(models.Model):
     name = models.CharField(max_length=128)
     number = models.BigIntegerField()
 
     def __str__(self):
-        return self.name
+        return '{0} ({1})'.format(self.name, self.number)
+
+
+    class Meta:
+        verbose_name = "BCC Receiver Email Address"
+        verbose_name_plural = "BCC Receiver Email Addresses"
+        default_permissions = ('view')
+
+
 
 class StaffName(models.Model):
     name = models.CharField(max_length=64,primary_key=True)
     def __str__(self):
-        return self.staffname
+        return self.name
+
+    class Meta:
+        verbose_name = "Enron Staff"
+        verbose_name_plural = "Enron Staffs"
+        default_permissions = ('view')
+
 
 
 class StaffEmail(models.Model):
@@ -95,4 +149,9 @@ class StaffEmail(models.Model):
     staffName = models.ForeignKey(StaffName, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.staffname + ":" + self.emailaddress
+        return self.staffName + ":" + self.emailAddress
+
+    class Meta:
+        verbose_name = "Enron Staff Email Address"
+        verbose_name_plural = "Enron Staff Email Addresses"
+        default_permissions = ('view')
