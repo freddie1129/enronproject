35ed443e4a95d9bde5bb36202844951381f3fb5e
Freddie <freddiechenchen@gmail.com>
2018-07-21 22:24:26 +1000

    a

--- scripts/emailconst.py
+++ scripts/emailconst.py
@@ -4,7 +4,7 @@ class mailConstant:
     result_received_email_number_from_enron_group = "received email number from staffs inside enron group"
     result_received_email_number_from_external = "received email number from person outside enron group"
     result_received_email_number_to_enron_group = "received email number to enron group"
-    result_received_email_number_to_external = "received email number to enron group"
+    result_received_email_number_to_external = "received email number to person outside enron group"
     result_received_email_number_between_enron_group = "received email number between enron group"
 
     string_unknown = "unkonwn"
