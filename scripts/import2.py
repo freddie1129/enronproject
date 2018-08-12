07c06af3d28a0203836184477a4aa7aa92c44f68
Freddie <freddiechenchen@gmail.com>
2018-08-12 22:38:15 +1000

    import

--- scripts/import2.py
+++ scripts/import2.py
@@ -4,13 +4,11 @@ from enron.models import RawEmail,RawEmailTo,RawEmailCc,RawEmailBCc
 
 def run():
     email_list = RawEmail.objects.all()
-    for email in email_list:
+    email_number = len(email_list)
+    i = 0
+    while i < email_number:
+        email = email_list[i]
         #str = "Tue, 28 Nov 2000 04:50:00 -0800 (PST)"
-        #re_date = re.compile(r"(?P<date>\w\w\w, \d{1,2} \w\w\w \d\d\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d).*")
-        #m = re_date.match(email.e_date.strip())
-        #str_date = m.group("date")
-        #mail_date = time.strftime("%Y-%m-%d %H:%M:%S", time.strptime(str_date, "%a, %d %b %Y %H:%M:%S %z"))
-
         re_date = re.compile(r"(?P<date>\w\w\w, \d{1,2} \w\w\w \d\d\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d).*")
         m = re_date.match(email.e_date.strip())
         str_date =  m.group("date")
@@ -27,3 +25,9 @@ def run():
         e_bcc_list = email.e_bc.strip(",")
         email_bcc_list = [RawEmailTo(e_id=email,e_date = mail_date, e_from=email.e_from.strip(),e_to = to_addr) for to_addr in e_bcc_list]
         RawEmailBCc.objects.bulk_create(email_bcc_list)
+
+        i += 1
+        if (i % 1000 == 0):
+            print("Number: {0}\n".format(i))
+
+    print("Processing End\n")
