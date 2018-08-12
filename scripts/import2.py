415ae4fb945092bd182008fbe71bed09ad1738d6
Freddie <freddiechenchen@gmail.com>
2018-08-12 23:11:09 +1000

    import

--- scripts/import2.py
+++ scripts/import2.py
@@ -1,6 +1,7 @@
 import re
 import time
 from enron.models import RawEmail,RawEmailTo,RawEmailCc,RawEmailBCc
+from django.core.exceptions import ValidationError
 
 def run():
     email_list = RawEmail.objects.all()
@@ -13,25 +14,28 @@ def run():
     while index < 517401:
         email = email_list[index]
         #str = "Tue, 28 Nov 2000 04:50:00 -0800 (PST)"
-        re_date = re.compile(r"(?P<date>\w\w\w, \d{1,2} \w\w\w \d\d\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d).*")
-        m = re_date.match(email.e_date.strip())
-        str_date =  m.group("date")
-        mail_date = time.strftime("%Y-%m-%d %H:%M:%S", time.strptime(str_date, "%a, %d %b %Y %H:%M:%S %z"))
+        try:
+            re_date = re.compile(r"(?P<date>\w\w\w, \d{1,2} \w\w\w \d\d\d\d \d\d:\d\d:\d\d [+-]\d\d\d\d).*")
+            m = re_date.match(email.e_date.strip())
+            str_date =  m.group("date")
+            mail_date = time.strftime("%Y-%m-%d %H:%M:%S", time.strptime(str_date, "%a, %d %b %Y %H:%M:%S %z"))
 
-        e_to_list = email.e_to.split(",")
-        email_to_list = [RawEmailTo(e_id=email,e_date = mail_date, e_from=email.e_from.strip(),e_to = to_addr) for to_addr in e_to_list]
-        RawEmailTo.objects.bulk_create(email_to_list)
+            e_to_list = email.e_to.split(",")
+            email_to_list = [RawEmailTo(e_id=email,e_date = mail_date, e_from=email.e_from.strip(),e_to = to_addr) for to_addr in e_to_list]
+            RawEmailTo.objects.bulk_create(email_to_list)
 
-        e_cc_list = email.e_cc.split(",")
-        email_cc_list = [RawEmailTo(e_id=email,e_date = mail_date, e_from=email.e_from.strip(),e_to = to_addr) for to_addr in e_cc_list]
-        RawEmailCc.objects.bulk_create(email_cc_list)
+            e_cc_list = email.e_cc.split(",")
+            email_cc_list = [RawEmailTo(e_id=email,e_date = mail_date, e_from=email.e_from.strip(),e_to = to_addr) for to_addr in e_cc_list]
+            RawEmailCc.objects.bulk_create(email_cc_list)
 
-        e_bcc_list = email.e_bcc.split(",")
-        email_bcc_list = [RawEmailTo(e_id=email,e_date = mail_date, e_from=email.e_from.strip(),e_to = to_addr) for to_addr in e_bcc_list]
-        RawEmailBCc.objects.bulk_create(email_bcc_list)
+            e_bcc_list = email.e_bcc.split(",")
+            email_bcc_list = [RawEmailTo(e_id=email,e_date = mail_date, e_from=email.e_from.strip(),e_to = to_addr) for to_addr in e_bcc_list]
+            RawEmailBCc.objects.bulk_create(email_bcc_list)
 
-        if (index % 1000 == 0):
-            print("Number: {0}\n".format(index))
+            if (index % 1000 == 0):
+                print("Number: {0}\n".format(index))
+        except ValidationError:
+            print("Error: {0}\n", email.e_id)
         index += 1
 
     print("Processing End\n")
